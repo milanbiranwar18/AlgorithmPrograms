9a8a246f9b3def127ab5fd4310018fe9e26c78c6
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-14 08:06:43 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgorithmPrograms
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            Console.WriteLine(" Wellcome to the Algorithm Problems");
+        }
+    }
+}
